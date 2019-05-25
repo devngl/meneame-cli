b3d324dcb4ff7f9db9be5d94ec8e7a96dac11995
ngldev <angel@messingbit.com>
2019-05-25 23:52:06 +0200

    OO method calling

--- app/Models/Traits/UuidKey.php
+++ app/Models/Traits/UuidKey.php
@@ -11,7 +11,7 @@ trait UuidKey
     {
         static::creating(static function (Model $model): void {
             if (!$model->getKey()) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
             }
         });
     }
